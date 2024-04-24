f8bbc8429b6b110e0c7b2c2e367d703dd0cc9825
Daniel Raper <me@danr.uk>
2024-04-24 10:28:46 +0100

    Bump version

--- custom_components/o2/const.py
+++ custom_components/o2/const.py
@@ -2,7 +2,7 @@ DOMAIN = "o2"
 CONFIG_VERSION = 1
 ENTITY_TYPES = ["sensor"]
 USER_AGENT = "dan-r-homeassistant-o2"
-INTEGRATION_VERSION = "0.1.1"
+INTEGRATION_VERSION = "0.1.3"
 
 DATA_COORDINATOR = "coordinator"
 DATA_APICLIENT = "apiclient"
